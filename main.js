6e370d468dc00573e384ffbf78d0812c8feaf03a
Jufebrown <jufebrown@gmail.com>
2017-01-12 11:35:11 -0600

    added sign in function

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-$('form').submit((e) => {
-  e.preventDefault()
-  var email = $('input[type="email"]').val()
-  //set to h1
-  $('.main-page h1').text(`Welcome ${email}`)
-  $('.login-page').addClass('hidden')
-  $('.main-page').removeClass('hidden')
-})
-
 // Initialize Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyA83A4Nz9ZO32NiPeNF8zdcYPTTPb3PYCQ",
@@ -14,4 +5,17 @@ firebase.initializeApp({
   databaseURL: "https://c17-firebase-auth-jufe.firebaseio.com",
   storageBucket: "c17-firebase-auth-jufe.appspot.com",
   messagingSenderId: "653707776406"
+})
+
+$('form').submit((e) => {
+  e.preventDefault()
+  var email = $('input[type="email"]').val()
+  var password = $('input[type="password"]').val()
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(() => {
+    //set to h1
+    $('.main-page h1').text(`Welcome ${email}`)
+    $('.login-page').addClass('hidden')
+    $('.main-page').removeClass('hidden')
+  })
 })
