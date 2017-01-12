3f66226b3c6737ec93dd5fdbfc2a40d7a3515bfe
Jufebrown <jufebrown@gmail.com>
2017-01-12 10:16:35 -0600

    changed submit function to hide show pages

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-$('form').submit(function(){
+$('form').submit((e) => {
+  e.preventDefault()
   var email = $('input[type="email"]').val()
+  //set to h1
+  $('.main-page h1').text(`Welcome ${email}`)
+  $('.login-page').addClass('hidden')
+  $('.main-page').removeClass('hidden')
 })
