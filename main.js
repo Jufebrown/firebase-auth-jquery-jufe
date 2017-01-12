c7c0024b8f2371ab0314fa47a769dc290d444bb4
Jufebrown <jufebrown@gmail.com>
2017-01-12 10:51:48 -0600

    initialized firebase

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ $('form').submit((e) => {
   $('.login-page').addClass('hidden')
   $('.main-page').removeClass('hidden')
 })
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyA83A4Nz9ZO32NiPeNF8zdcYPTTPb3PYCQ",
+  authDomain: "c17-firebase-auth-jufe.firebaseapp.com",
+  databaseURL: "https://c17-firebase-auth-jufe.firebaseio.com",
+  storageBucket: "c17-firebase-auth-jufe.appspot.com",
+  messagingSenderId: "653707776406"
+})
